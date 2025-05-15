cb492210c15e908a77f591256825d5e4786a01be
schAKura <alan.kuranowski@tm1.edu.pl>
2025-05-15 10:25:21 +0200

    modified:   script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ class Card {
     }
 }
 
-class Talia {
+class Deck {
     constructor() {
         this.cards = [];
         const kolory = ["Kier", "Karo", "Trefl", "Pik"];
@@ -28,13 +28,45 @@ class Talia {
     }
 }
 
-function shuffle(talia) {
-    for (let i = talia.cards.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [talia.cards[i], talia.cards[j]] = [talia.cards[j], talia.cards[i]]; // Zamiana miejsc
+class Player{
+    constructor() {
+        this.cards = [];
+        this.points = 0;
+    }
+
+    calcPoints() {
+        for(let card of this.cards){
+            this.points += card.wartosc;
+        }
     }
 }
 
+function shuffle(deck) {
+    for (let i = deck.cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck.cards[i], deck.cards[j]] = [deck.cards[j], deck.cards[i]]; // Zamiana miejsc
+    }
+}
+
+var player = new Player();
+var dealer = [];
+var deck = new Deck();
+
 function start() {
+    shuffle(deck);
+
+    player.cards.push(deck.cards[0]);
+    player.cards.push(deck.cards[1]);
+    dealer.push(deck.cards[2]);
+    dealer.push(deck.cards[3]);
+    for(let i=0; i<4; i++)
+        deck.cards.shift();
+}
+
+function hit() {
+
+}
+
+function stand() {
 
 }
