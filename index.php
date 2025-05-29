c101877f887dbb40238f561f04368c1e04e07abe
Skibidi <awd@gmail.com>
2025-05-29 09:51:12 +0200

    adaw

--- index.php
+++ index.php
@@ -10,13 +10,23 @@
 <body>
 <h1>Blackjack</h1>
 <div class="stol"></div>
-<img src="../BlackJack/img/potencjalne_karty-removebg-preview.png" alt="arsenal" class="karty_arsenal">
-<img src="../BlackJack/img/karta-removebg-preview.png" alt="karty_do_gry" class="karty_do_gry">
+<div class="karta_k">
+    <div class="kwadrat1">K</div>
+    <div class="kwadrat2">K</div>
+    <div class="kwadrat3">K</div>
+</div>
+<div class="karta_a">
+    <div class="kwadrat1">A</div>
+    <div class="kwadrat2">A</div>
+    <div class="kwadrat3">A</div>
+</div>
+
 <button class="start" onclick="start()">Rozpocznij grę</button>
 <div class="zeton_1"><div class="pod_zeton"><b>10</b></div></div>
 <div class="zeton_2"><div class="pod_zeton"><b>50</b></div></div>
 <div class="zeton_3"><div class="pod_zeton"><b>100</b></div></div>
 <div class="zeton_4"><div class="pod_zeton"><b>500</b></div></div>
+<h1 class="wynik">Wynik:</h1>
 <button class="hit" onclick="hit()">Hit</button>
 <button class="stand" onclick="stand()">Stand</button>
 <button class="how_to">Help</button>
