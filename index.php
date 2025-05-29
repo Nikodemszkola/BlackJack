26d201392893364e8a3bdd92c8be5b30a94c8152
Skibidi <awd@gmail.com>
2025-05-29 10:07:30 +0200

    adawd

--- index.php
+++ index.php
@@ -20,13 +20,21 @@
     <div class="kwadrat2">A</div>
     <div class="kwadrat3">A</div>
 </div>
-
+<table>
+    <tr>
+        <th>Stawka:</th>
+    </tr>
+    <tr>
+        <td>1000np</td>
+    </tr>
+</table>
 <button class="start" onclick="start()">Rozpocznij grę</button>
 <div class="zeton_1"><div class="pod_zeton"><b>10</b></div></div>
 <div class="zeton_2"><div class="pod_zeton"><b>50</b></div></div>
 <div class="zeton_3"><div class="pod_zeton"><b>100</b></div></div>
 <div class="zeton_4"><div class="pod_zeton"><b>500</b></div></div>
 <h1 class="wynik">Wynik:</h1>
+<h1 class="wynik2">Wynik:</h1>
 <button class="hit" onclick="hit()">Hit</button>
 <button class="stand" onclick="stand()">Stand</button>
 <button class="how_to">Help</button>
